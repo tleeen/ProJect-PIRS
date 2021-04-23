93c1a49331d8c71dcb3bc18f28e2d9241f91612c
Ivanov Peter <peterivan5555@gmail.com>
2021-04-23 17:07:04 +0700

    splash_screen

--- PIRS/gui_v.2.py
+++ PIRS/gui_v.2.py
@@ -1,12 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QSystemTrayIcon, QAction, qApp, QMenu, QGraphicsDropShadowEffect
 from PyQt5 import QtGui, uic
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QApplication, QWidget, QSystemTrayIcon, QAction, qApp, QMenu, QGraphicsDropShadowEffect, \
+    QMainWindow
 from pycaw.pycaw import AudioUtilities
+from ui_splash_screen import Ui_SplashScreen
 from Assistant import *
 
-class App(QWidget):
+## ==> GLOBALS
+counter = 0
 
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -137,8 +142,63 @@ class App(QWidget):
             self.settings.warning.setText("Не все поля были заполнены")
 
 
+# SPLASH SCREEN
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_SplashScreen()
+        self.ui.setupUi(self)
+
+        ## UI ==> INTERFACE CODES
+        ########################################################################
+
+        ## REMOVE TITLE BAR
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        ## DROP SHADOW EFFECT
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 60))
+        self.ui.dropShadowFrame.setGraphicsEffect(self.shadow)
+
+        ## QTIMER ==> START
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        # TIMER IN MILLISECONDS
+        self.timer.start(35)
+
+        ## SHOW ==> MAIN WINDOW
+        ########################################################################
+        self.show()
+        ## ==> END ##
+
+    ## ==> APP FUNCTIONS
+    ########################################################################
+    def progress(self):
+
+        global counter
+
+        # SET VALUE TO PROGRESS BAR
+        self.ui.progressBar.setValue(counter)
+
+        # CLOSE SPLASH SCREE AND OPEN APP
+        if counter > 100:
+            # STOP TIMER
+            self.timer.stop()
+
+            #SHOW MAIN WINDOW
+            self.main = MainWindow()
+
+            # CLOSE SPLASH SCREEN
+            self.close()
+
+        # INCREASE COUNTER
+        counter += 1
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
-    # Assistant.greeting()
-    app.exec()
+    window = SplashScreen()
+    sys.exit(app.exec_())
